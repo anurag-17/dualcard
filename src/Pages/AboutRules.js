70593d76c35f76b41f7581135931a327c5f5e018
Anurag Pandey <anurag.pandey17@gmail.com>
2022-08-24 10:27:46 +0530

    first

---
+++ src/Pages/AboutRules.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutRules = () => {
+  return (
+    <div>AboutRules</div>
+  )
+}
+
+export default AboutRules
---
+++ src/Pages/ICOInformation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ICOInformation = () => {
+  return (
+    <div>ICOInformation</div>
+  )
+}
+
+export default ICOInformation
