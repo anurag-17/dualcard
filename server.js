d7b594a9822df68f3b9781b3f71f2baf092bfe51
Anurag Pandey <anurag.pandey17@gmail.com>
2022-08-26 11:08:09 +0530

    new build

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ if (process.env.NODE_ENV === "production") {
 } 
 else {
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-
+    res.send("API is running..");
   });
 }
 
