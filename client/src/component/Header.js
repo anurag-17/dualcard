b34d57e53fe92214ac7d4bd47e510c6f936a1236
Anurag Pandey <anurag.pandey17@gmail.com>
2022-08-26 11:51:48 +0530

    Update Header.js

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -21,7 +21,7 @@ console.log(data)
          <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to = "/">
-                    <img src='./Logo.png'></img>
+                    <img src='/Logo.png'></img>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
