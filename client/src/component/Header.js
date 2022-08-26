6d6f54a81f4230b6f6ca75430976ae709ffa85c8
Anurag Pandey <anurag.pandey17@gmail.com>
2022-08-26 19:51:56 +0530

    logo

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -26,7 +26,7 @@ const logoutuser = () => {
          <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to = "/">
-                    <img src={'/Logo.png'} alt=""></img>
+                    <img src='./Logo.png' alt=""></img>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
