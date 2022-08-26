f0194ca2c11ca1b620a434d1bb25cfbb08e66297
Anurag Pandey <anurag.pandey17@gmail.com>
2022-08-26 11:33:33 +0530

    server changes

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.post("/upload",(req,res)=>{
 
 
 // --------------------------deployment------------------------------
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
+if (process.env.NODE_ENV === "production") {
 } 
 else {
   app.get("/", (req, res) => {
