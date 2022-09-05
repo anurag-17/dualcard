db914739c94d1750f73ef7bc2ba89d0c06cebdf5
Anurag Pandey <anurag.pandey17@gmail.com>
2022-09-05 17:58:19 +0530

    commit duel accepted changes

--- client/src/component/DuelAccepted.js
+++ client/src/component/DuelAccepted.js
@@ -3,6 +3,7 @@ import './DuelAccepted.css';
 import Carousel from 'react-bootstrap/Carousel';
 import axios from "axios"
 import {Link} from "react-router-dom"
+import { Loader } from './Loader';
 
 
 export const DuelAccepted = () => {
@@ -11,7 +12,6 @@ export const DuelAccepted = () => {
     const [challengeid,setChallengeId] = useState("")
     const [loader,setLoader] = useState(true)
     const [userimagedata, setuserimagedata] = useState([]);
-
     const data = JSON.parse(localStorage.getItem("nftuser"));
     const id = data._id;
 
@@ -48,112 +48,125 @@ export const DuelAccepted = () => {
         getrecieved()
       },[])
 
+setTimeout(()=>{
+setLoader(false)
+},1000)
+
   return (
     <div>
-        <div className='duelacept-sec'>
-            <div className='container'>
-                <div className='section-title'>
-                    <h2 className = "body-main">Duel Accepted</h2>
+      {
+        loader?<Loader/>:<div className='duelacept-sec'>
+        <div className='container'>
+            <div className='section-title'>
+                <h2 className = "body-main">Duel Accepted</h2>
+            </div>
+
+            <div className='row duelat-main'>
+
+                {
+                    challengedata.map((items,index)=>{
+                        console.log(items)
+                        return(
+                            <>
+                            
+
+                <div className='col-md-5 col-sm-5'>
+
+                   <div className='dA-left'>
+                    <div className='dA-profile'>
+                        <div class="clearfix">
+                                <img src="./tabicon8.png" alt="img"/>
+                                <button type="button" class="btn float-end">{items.player_1[0].name}</button>
+                        </div> 
+                        <div className='dA-slider'>
+
+                        <Carousel>
+                        {
+                items.player_1[0].images.map((items, index) => {
+                    return (
+                        <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src={items}
+                          alt="First slide"
+                          
+                        />
+                        </Carousel.Item>
+                      );
+                    
+                })
+            }
+                
+                    </Carousel>
+                        </div>
+                        <div className='select-btn'>
+                          <button className='hero-btn'> <span>{items.player_1[0].images.length}</span>selected</button>
+                        </div>
+                        <div className='btn-duel-right winner-btn'>
+                            <Link to = "/winner">
+                                <button className='hero-btn'>Winner</button>
+                            </Link>
+                            <Link to = "/loser">
+                                <button className='hero-btn'>Loser</button>
+                            </Link>
+                         </div>
+                    </div>
+                    </div> 
                 </div>
+                <div className='col-md-2 col-sm-2 duelA-center'>
+                   <img src="./VS icon.png" alt="img"/>
+                </div>
+                <div className='col-md-5 col-sm-5'>
+                <div className='dA-left'>
+                    <div className='dA-profile'>
+                    <div class="clearfix">
+                            <img src="./tabicon-6.png" alt="img"/>
+                            <button type="button" class="btn float-end">{items.player_2[0].name}</button>
+                     </div> 
+                     <div className='dA-slider'>
 
-                <div className='row duelat-main'>
-
-                    {
-                        challengedata.map((items,index)=>{
-                            console.log(items)
-                            return(
-                                <>
-                                
-
-                    <div className='col-md-5 col-sm-5'>
-
-                       <div className='dA-left'>
-                        <div className='dA-profile'>
-                            <div class="clearfix">
-                                    <img src="./tabicon8.png" alt="img"/>
-                                    <button type="button" class="btn float-end">{items.player_1[0].name}</button>
-                            </div> 
-                            <div className='dA-slider'>
-
-                            <Carousel>
+                        <Carousel>
                             {
-                    items.player_1[0].images.map((items, index) => {
-                        return (
-                            <Carousel.Item>
-                            <img
-                              className="d-block w-100"
-                              src={items}
-                              alt="First slide"
-                              
-                            />
-                            </Carousel.Item>
-                          );
-                        
+                                userimagedata.map((items,index)=>{
+                                    return(
+                                        <Carousel.Item>
+                                        <img
+                                        className="d-block w-100"
+                                        src={items.url}
+                                        alt="First slide"
+                                        />
+                                        <Carousel.Caption>
+                                        
+                                        </Carousel.Caption>
+                                    </Carousel.Item>
+                                    )
+                                })
+                            }
+                    </Carousel>
+                        </div>
+                        <div className='select-btn'>
+                          <button className='hero-btn'> <span>3</span>selected</button>
+                        </div>
+                        <div className='btn-duel-right winner-btn'>
+                            <Link to = "/winner">
+                                <button className='hero-btn'>Winner</button>
+                            </Link>
+                            <Link to = "/loser">
+                                <button className='hero-btn'>Loser</button>
+                            </Link>
+                         </div>
+                    </div>
+                    </div> 
+               </div>
+                            </>
+                        )
                     })
                 }
-                    
-                        </Carousel>
-                            </div>
-                            <div className='select-btn'>
-                              <button className='hero-btn'> <span>{items.player_1[0].images.length}</span>selected</button>
-                            </div>
-                            <div className='btn-duel-right winner-btn'>
-                                <Link to = "/winner">
-                                    <button className='hero-btn'>Winner</button>
-                                </Link>
-                             </div>
-                        </div>
-                        </div> 
-                    </div>
-                    <div className='col-md-2 col-sm-2 duelA-center'>
-                       <img src="./VS icon.png" alt="img"/>
-                    </div>
-                    <div className='col-md-5 col-sm-5'>
-                    <div className='dA-left'>
-                        <div className='dA-profile'>
-                        <div class="clearfix">
-                                <img src="./tabicon-6.png" alt="img"/>
-                                <button type="button" class="btn float-end">{items.player_2[0].name}</button>
-                         </div> 
-                         <div className='dA-slider'>
-
-                            <Carousel>
-                                {
-                                    userimagedata.map((items,index)=>{
-                                        return(
-                                            <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src={items.url}
-                                            alt="First slide"
-                                            />
-                                            <Carousel.Caption>
-                                            
-                                            </Carousel.Caption>
-                                        </Carousel.Item>
-                                        )
-                                    })
-                                }
-                        </Carousel>
-                            </div>
-                            <div className='select-btn'>
-                              <button className='hero-btn'> <span>3</span>selected</button>
-                            </div>
-                            <div className='btn-duel-right winner-btn'>
-                            <Link to = "/loser">
-                                    <button className='hero-btn'>Loser</button>
-                                </Link>
-                             </div>
-                        </div>
-                        </div> 
-                   </div>
-                                </>
-                            )
-                        })
-                    }
-                 </div>
-            </div>
+             </div>
         </div>
+    </div>
+      }
+        
     </div>
   )
 }
